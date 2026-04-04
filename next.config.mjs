d15140526d7b32b19217a2b5a4de38efd260b1d0
Sangeeth-Mohan <sangeethkumarmohan@gmail.com>
2026-04-04 12:58:55 -0400

    fix: switch to SSR mode for Amplify Web Compute

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',   // Enables static export for AWS S3/Amplify
-  trailingSlash: true,
   images: {
-    unoptimized: true, // Required for static export
+    unoptimized: true,
   },
 }
 
